171626ca41428f59daf212c255aa8deabfc33198
shanu1195 <shanu1195@Trickster.github.com>
2021-01-01 19:12:45 +0530

    Add project files.

---
+++ CM_HangMan/CM_HangMan.cpp
@@ -0,0 +1,12 @@
+﻿// CM_HangMan.cpp : Defines the entry point for the application.
+//
+
+#include "CM_HangMan.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
